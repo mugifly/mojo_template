7875ba979f5658018925dd67923d6cf4aeec8d1a
Masanori Ohgita <piyo3728@gmail.com>
2013-01-05 16:27:12 +0900

    /Helper/Login.pm - Fix for Twitter OAuth2

--- login_site/lib/Example/Helper/Login.pm
+++ login_site/lib/Example/Helper/Login.pm
@@ -17,7 +17,8 @@ sub register {
 				site	=>	'https://oauth.twitter.com',
 				authorize_path	=>	'/2/authorize',
 				access_token_path=>	'/o/oauth2/token',
-			)->web_server(redirect_uri => ($app->config()->{base_url}) .'session/oauth_twitter_callback');
+			)->web_server(oauth_client_identifier => $app->config()->{oauth_twitter_key},
+				oauth_callback_url => ($app->config()->{base_url}) .'session/oauth_twitter_callback');
 		}
 	);
 }
